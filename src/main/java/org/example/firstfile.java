2b7ca8ab06dad26c8706a407444934e7808b5dfb
SuyogNe <suyogn78@gmail.com>
2026-03-30 15:33:14 +0545

    first java file

---
+++ src/main/java/org/example/firstfile.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.SortedMap;
+
+public class firstfile {
+
+    public static void main(String[] args){
+
+        int x = 10;
+        int y = 13;
+        String abcd = "Hello World!";
+
+
+        System.out.println("First number:"+x);
+        System.out.println("Second number:"+y);
+        System.out.println("The sum of numbers:"+ (x + y));
+        System.out.println(abcd);
+}}
