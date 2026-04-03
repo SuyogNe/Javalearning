ff3e385b290ca63a21a33f88e77746a7acc2ee1f
SuyogNe <suyogn78@gmail.com>
2026-04-03 21:12:28 +0545

    example java

---
+++ src/main/java/org/example/secondfile.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class secondfile {
+    public static void main(String[] args) {
+
+
+        int x = 10;
+        int y = 20;
+        float z = 45.3f;
+        String name = "Arshdeep Singh";
+        boolean isBumrahabowler = true;
+        boolean isRohitabowler = false;
+        char ch = 'S';
+        System.out.println("Sum of x and y:" + (x + y));
+        System.out.println("fast bowler name:" + name);
+        System.out.println("Float z + Integer x:" + (x + z));
+    }}
